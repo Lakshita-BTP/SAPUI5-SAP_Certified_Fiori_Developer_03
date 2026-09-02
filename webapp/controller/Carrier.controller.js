73db61a9250e94c4b9fc22e625c53aa47fb0c253
Jhony Gautam <lakshita.flow@gmail.com>
2026-09-02 10:18:25 +0000

    02. vi). Implementing View Synchronization

--- webapp/controller/Carrier.controller.js
+++ webapp/controller/Carrier.controller.js
@@ -1,12 +1,50 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "student/com/sap/training/advancedsapui5/fullscreen/controller/BaseController"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("student.com.sap.training.advancedsapui5.fullscreen.controller.Carrier", {
-        onInit: function () {
+        return Controller.extend("student.com.sap.training.advancedsapui5.fullscreen.controller.Carrier", {
 
-        }
+            onInit: function () {
+                var oList = this.byId("list");
+                this._oList = oList;
+                this.getView().addEventDelegate({
+                    onBeforeFirstShow: function () {
+                        this.getOwnerComponent().oListSelector.setBoundMasterList(this._oList);
+                    }.bind(this)
+                });
+                this.getRouter().getRoute("Overview").attachPatternMatched(this._onListMatched, this);
+                this.getRouter().attachBypassed(this.onBypassed, this);
+            },
+
+            _navigateToCarrierDetails: function (sCarrierId, bReplace) {
+                this.getRouter().navTo("flights", {
+                    carrid: sCarrierId
+                }, bReplace);
+            },
+
+            _showDetail: function (oItem) {
+                var sCarrierId = oItem.getBindingContext().getProperty("Carrid");
+                this._navigateToCarrierDetails(sCarrierId, true);
+            },
+
+            onSelect: function (oEvent) {
+                this._showDetail(oEvent.getParameter("listItem") || oEvent.getSource());
+            },
+
+            onBypassed: function () {
+                this._oList.removeSelections(true);
+            },
+
+            _onListMatched: function () {
+                this.getListSelector().oWhenListLoadingIsDone.then(
+                    function (mParams) {
+                        var sObjectId = mParams.oFirstListItem.getBindingContext().getProperty("Carrid");
+                        this._navigateToCarrierDetails(sObjectId, true);
+                    }.bind(this)
+                );
+            }
+
+        });
     });
-});
--- webapp/controller/Flights.controller.js
+++ webapp/controller/Flights.controller.js
@@ -1,12 +1,50 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "student/com/sap/training/advancedsapui5/fullscreen/controller/BaseController"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("student.com.sap.training.advancedsapui5.fullscreen.controller.Flights", {
-        onInit: function () {
+        return Controller.extend("student.com.sap.training.advancedsapui5.fullscreen.controller.Flights", {
 
-        }
+            onInit: function () {
+                var oRouter = this.getRouter();
+                oRouter.getRoute("flights").attachMatched(this._onObjectMatched, this);
+
+                var oModel = this.getOwnerComponent().getModel();
+                oModel.attachRequestSent(function (oEvent) {
+                    this.getView().setBusy(true);
+                }, this);
+                oModel.attachRequestCompleted(function (oEvent) {
+                    this.getView().setBusy(false);
+                }, this);
+            },
+
+            _onBindingChange: function () {
+                var oView = this.getView();
+                var oElementBinding = oView.getElementBinding();
+                if (!oElementBinding.getBoundContext()) {
+                    this.getRouter().getTargets().display("notFound");
+                    this.getOwnerComponent().oListSelector.clearMasterListSelection();
+                    return;
+                }
+                var sPath = oElementBinding.getPath();
+                this.getOwnerComponent().oListSelector.selectAListItem(sPath);
+            },
+
+            _onObjectMatched: function (oEvent) {
+                var oArgs = oEvent.getParameter("arguments");
+                this._sCarrierId = oArgs.carrid;
+                var oView = this.getView();
+
+                oView.getModel("mainView").setProperty("/layout", "TwoColumnsMidExpanded");
+
+                oView.bindElement({
+                    path: "/UX_C_Carrier_TP('" + this._sCarrierId + "')",
+                    events: {
+                        change: this._onBindingChange.bind(this)
+                    }
+                });
+            },
+
+        });
     });
-});
